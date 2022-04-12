1b2325fe1766cec04f0b398cbd424e7aaa84eb9c
Josh Archer <jarcher@greenriver.edu>
2022-04-12 11:10:03 -0700

    Added an example of using the PriorityQueue class in the Java libraries.

---
+++ priority_queues/src/model/Airplane.java
@@ -0,0 +1,60 @@
+package model;
+
+public class Airplane implements Comparable<Airplane>
+{
+    private String planeId;
+    private double fuelRemaining; //gallons
+
+    public Airplane(String planeId, double fuelRemaining)
+    {
+        this.planeId = planeId;
+        this.fuelRemaining = fuelRemaining;
+    }
+
+    @Override
+    public int compareTo(Airplane other)
+    {
+        if (this.fuelRemaining < other.fuelRemaining)
+        {
+            return -1;
+        }
+        else if (this.fuelRemaining > other.fuelRemaining)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+        /*return this.planeId.compareTo(other.planeId);*/
+    }
+
+    public String getPlaneId()
+    {
+        return planeId;
+    }
+
+    public double getFuelRemaining()
+    {
+        return fuelRemaining;
+    }
+
+    public void setPlaneId(String planeId)
+    {
+        this.planeId = planeId;
+    }
+
+    public void setFuelRemaining(double fuelRemaining)
+    {
+        this.fuelRemaining = fuelRemaining;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Airplane{" +
+                "planeId='" + planeId + '\'' +
+                ", fuelRemaining=" + fuelRemaining +
+                '}';
+    }
+}
---
+++ priority_queues/src/test/TestPriorityQueue.java
@@ -0,0 +1,55 @@
+package test;
+
+import model.Airplane;
+
+import java.util.PriorityQueue;
+
+public class TestPriorityQueue
+{
+    public static void main(String[] args)
+    {
+        PriorityQueue<Airplane> airTrafficController = new PriorityQueue<>();
+
+        //add a few planes
+        Airplane chicagoFlight = new Airplane("CHI 222", 400.0);
+
+        airTrafficController.add(new Airplane("SEA 101", 500.0));
+        airTrafficController.add(new Airplane("SEA 202", 250.0));
+        airTrafficController.add(new Airplane("TAC 303", 750.0));
+        airTrafficController.add(new Airplane("LOS 198", 150.0));
+        airTrafficController.add(new Airplane("LOS 199", 300.0));
+        airTrafficController.add(chicagoFlight);
+        airTrafficController.add(new Airplane("CHI 333", 500.0));
+
+        //then land them one by one
+        /*while (!airTrafficController.isEmpty())
+        {
+            //land a single plane
+            Airplane plane = airTrafficController.poll();
+            System.out.println("Plane landed: " + plane.getPlaneId());
+        }*/
+
+        //we have space to land three planes
+        for (int i = 1; i <= 3; i++)
+        {
+            Airplane plane = airTrafficController.poll();
+            System.out.println("Plane landed: " + plane.getPlaneId());
+        }
+
+        //a few more planes arrive
+        airTrafficController.add(new Airplane("PORT 104", 90.0));
+        airTrafficController.add(new Airplane("PORT 204", 190.0));
+
+        //a plane dumps some of it's fuel
+        airTrafficController.remove(chicagoFlight);
+        chicagoFlight.setFuelRemaining(50.0);
+        airTrafficController.add(chicagoFlight);
+
+        //we have space to land two planes
+        for (int i = 1; i <= 2; i++)
+        {
+            Airplane plane = airTrafficController.poll();
+            System.out.println("Plane landed: " + plane.getPlaneId());
+        }
+    }
+}
